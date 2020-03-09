e9354b67ef21fe881a40c2a0eb0f0b2965ace319
ainhoarey <ainhoa1_rey@hotmail.com>
2020-03-09 20:05:39 +0100

    .

--- src/dssmoles/Patient.java
+++ src/dssmoles/Patient.java
@@ -33,7 +33,7 @@ public class Patient {
     }
 
     public Patient() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public String getName() {
